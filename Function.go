2fb561d3a8a8e1ee1cd22a0c30430c419437b4bf
BitMasher <61257372+BitMasher@users.noreply.github.com>
2020-04-14 20:46:28 -0500

    added twitch webhook stream info model

--- Function.go
+++ Function.go
@@ -4,23 +4,42 @@ package p
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 )
+
+type twitchStreamInfo struct {
+	GameId       string `json:"game_id"`
+	Id           string `json:"id"`
+	Language     string `json:"language"`
+	Pagination   string `json:"pagination"`
+	StartedAt    string `json:"started_at"`
+	TagIds       string `json:"tag_ids"`
+	ThumbnailUrl string `json:"thumbnail_url"`
+	Title        string `json:"title"`
+	StreamType   string `json:"type"`
+	UserId       string `json:"user_id"`
+	UserName     string `json:"user_name"`
+	ViewerCount  int    `json:"viewer_count"`
+}
+
+type twitchPayload struct {
+	Data []twitchStreamInfo
+}
 
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func TwitchWebhook(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-	}
+	userId := r.URL.Query().Get("userid")
+	var d twitchPayload
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
+		if len(d.Data) > 0 {
+			for i := range d.Data {
+				fmt.Printf("Stream for %s is %s\n", d.Data[i].UserName, d.Data[i].StreamType)
+				fmt.Fprintf(w, "Stream for %s is %s\n", d.Data[i].UserName, d.Data[i].StreamType)
+			}
+		} else {
+			fmt.Fprintf(w, "Stream for %s is offline\n", userId)
+		}
 		return
 	}
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
-		return
-	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
 }
